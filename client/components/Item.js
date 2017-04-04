8938a1f58c6a80ccfde4895fe95e47cc4daa9f77
Ali Alrawi <ali.developer90@gmail.com>
2017-04-04 17:23:12 +0300

    set skills to the SkillCategory Item

--- client/components/Item.js
+++ client/components/Item.js
@@ -4,12 +4,12 @@ const SkillCategory = require('./SkillCategory');
 class Item extends React.Component {
   constructor(props) {
     super(props)
-    this.state = { categoryName:'default' }
+    this.state = { categoryName:'' ,skills:[]}
      this.handler = this.handler.bind(this)
   }
-     handler(cn) {
+     handler(cn,skills) {
     this.setState({
-      categoryName: cn
+      categoryName: cn,skills:skills
     })
   }
   render() {
@@ -18,7 +18,8 @@ class Item extends React.Component {
 
         <div>
 
-         <label>Skill Category Name: <i>{this.state.categoryName}</i> </label>
+         <label>Skill Category Name: <i>{this.state.categoryName}</i> </label><br />
+         <label>Skills: <i>{this.state.skills.join(', ')}</i> </label>
           <SkillCategory handler={this.handler} />
 
         </div>
