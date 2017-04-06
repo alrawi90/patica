912693e5a78699b0040ebf046ccc80ec79e1a551
Ali Alrawi <ali.developer90@gmail.com>
2017-04-06 13:13:45 +0300

    add LegoGallery class

---
+++ client/components/LegoGallery.js
@@ -0,0 +1,74 @@
+const React = require('react');
+var Swipeable = require('react-swipeable')
+var Row = require('react-bootstrap').Row;
+var Col=require('react-bootstrap').Col;
+var Thumbnail=require('react-bootstrap').Thumbnail;
+
+class LegoGallery extends React.Component{
+
+    constructor(props){
+      super(props)
+      this.handleClick=this.handleClick.bind(this)
+      this.handleNext=this.handleNext.bind(this)
+      this.handlePrevious=this.handlePrevious.bind(this)
+      this.state={
+        key:Date.now()
+         items:[
+
+               ],
+         url:'./client/assets/icons/1.png',
+         counter:0
+       }
+   }
+    handleClick(e){
+      let index=e.target.id.split('-')[1] -1
+      this.props.pickedLego(e.target.src);
+      this.setState({
+          counter:index
+      })
+    }
+    handlePrevious(e){this.state.counter <= 0 ? this.setState({ counter: 0}) :  this.setState({ counter: this.state.counter - 1})}
+    handleNext(e){this.state.counter >= this.state.items.length -1 ? this.setState({ counter: this.state.items.length -1}) : this.setState({ counter: this.state.counter + 1})}
+
+    render(){
+      const thumbs=this.state.items.map((item,index)=>{
+        return(
+            <div key={this.state.key} className='thumb' >
+               <img id={`thumb-${index+1}`} onClick={this.handleClick} style={{height:'75px',width:'75px'}} src={this.state.items[this.state.counter]} />
+            </div>
+        )
+
+      })
+     return(
+
+       <Swipeable
+          onSwiping={this.swiping}
+          onSwipingUp={this.swipingUp}
+          onSwipingRight={this.swipingRight}
+          onSwipingDown={this.swipingDown}
+          onSwipingLeft={this.swipingLeft}
+          onSwipedUp={this.swipedUp}
+          onSwipedRight={this.swipedRight}
+          onSwipedDown={this.swipedDown}
+          onSwipedLeft={this.swipedLeft}
+          onSwiped={this.handleSwipeAction}>
+
+          <div className='lego-gallery-container' >
+            <div id='lego-gallery-Lbtn-container'>
+              <button onClick={this.handleNext} >{'<'}</button>
+            <div>
+            <div className='lego-gallery-thumbs-container'>
+              {thumbs}
+            </div>
+            <div>
+              <button id='lego-gallery-Rbtn-container'  onClick={this.handlePrevious} >{'>'}</button>
+            </div>
+            </div>
+
+        </Swipeable>
+
+     )
+   }
+
+}
+module.exports = LegoGallery
