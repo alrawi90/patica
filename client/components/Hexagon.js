ed50faa8bda83b880ad085dbb6c5d0843524d031
Ali Alrawi <ali.developer90@gmail.com>
2017-04-08 09:26:44 +0300

    update Hexagon: added bg-color effect along with bg-image

--- client/components/Hexagon.js
+++ client/components/Hexagon.js
@@ -23,14 +23,16 @@ class Hexagon extends React.Component {
           version="1.1" width={this.props.size+'em'} height={this.props.size+'em'}
           xmlnsXlink="http://www.w3.org/1999/xlink">
 
-          <defs key={this.state.key}>
+          <defs key={this.state.key} >
             <pattern id={imageKey} x="0" y="0"
               height="300" width="300"
               patternUnits="userSpaceOnUse">
               <image width="300" height="300" xlinkHref={this.state.url}></image>
             </pattern>
           </defs>
-
+          <polygon
+            className="hex" points="300,150 225,280 75,280 0,150 75,20 225,20"
+            style={{fill:'lime',stroke:'purple',strokeWidth:1}}></polygon>
           <polygon onClick={this.props.click}
             className="hex" points="300,150 225,280 75,280 0,150 75,20 225,20"
             fill={imageUrl}></polygon>
