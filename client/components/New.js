49688be45a74dd8bdae676329e3fe0c3a657733d
Ali Alrawi <ali.developer90@gmail.com>
2017-04-10 21:55:15 +0300

    setting New.js to create post api call(create role)

--- client/components/New.js
+++ client/components/New.js
@@ -1,5 +1,4 @@
 
-
 const React = require('react');
 const SkillCategoryLabel = require('./SkillCategoryLabel');
 const RoleLabel = require('./RoleLabel');
@@ -9,6 +8,7 @@ class App extends React.Component {
   constructor(props) {
     super(props)
     this.state={
+      error:'' ,
       categories:[
       {skillCategoryName:'Category Name',skills:['skill-1','skill-2','skill-3'],categoryImg:'client/assets/icons/1.png'},
       {skillCategoryName:'Category Name',skills:['skill-1','skill-2','skill-3'],categoryImg:''},
@@ -23,6 +23,7 @@ class App extends React.Component {
     this.setRoleProps=this.setRoleProps.bind(this)
     this.setCategoryDetails=this.setCategoryDetails.bind(this)
     this.setLego=this.setLego.bind(this)
+    this.create=this.create.bind(this)
   }
   setRoleProps(roleName_,keywords_,description_){
 
@@ -47,11 +48,12 @@ class App extends React.Component {
   componentDidUpdate(){console.log(this.state.categories[0].categoryImg)}
 
   create(){
+      console.log('posting .....')
       const opts={}
       opts.name=this.state.roleName
       opts.description=this.state.description
       opts.image=this.state.lego
-      opts.keywords=this.state.keywords.map((item,value)=>{return(value)})//['a','b','c']
+      opts.keywords=this.state.keywords.map((item,index)=>{return(item.text)})//['a','b','c']
       opts.skillCategory=
           [
             // {
@@ -60,14 +62,14 @@ class App extends React.Component {
             //   image:this.state.categories[0].categoryImg
             // }
           ]
-      opts.skillCategory=[1,2,3,4,5,6,7].map((item,index)=>{
+      opts.skillCategory=[1,2,3,4,5,6].map((item,index)=>{
         return(
         {         name:this.state.categories[index].skillCategoryName,
                   skills:this.state.categories[index].skills,
                   image:this.state.categories[index].categoryImg
                 }
       )})
-
+      const app=this
       fetch('http://localhost:3002/role', {
        method: 'post',
        headers: {
@@ -78,54 +80,81 @@ class App extends React.Component {
            }).then(function(response) {
              return response.json();
            }).then(function(data) {
-             console.log('Created Row:', data);
+              app.setState({ error: data.error.message });
+
      });
   }
-
   render() {
-    const template = [1,2,3,4,5,6,7].map((id,index)=>{
-      return(
-       if(id <4){
+
+    return (
+    <div>
+      <ReactBootstrap.Grid >
+      <ReactBootstrap.Row >
+      <ReactBootstrap.Col md={12}>
+       <RoleLabel setRoleProps={this.setRoleProps} roleName={this.state.roleName} keywords={this.state.keywords}
+         description={this.state.description} suggestions={this.state.suggestions}
+       />
+      </ReactBootstrap.Col>
+      </ReactBootstrap.Row>
+      <div style={{display:'flex',flexDirection:'row',justifyContent: 'space-around'}}>
          <div style={{display:'flex',flexDirection:'column',justifyContent: 'space-between'}} >
-         <SkillCategoryLabel
-             id={id}
-             setCategoryDetails={this.setCategoryDetails}
-             skills={this.state.categories[index].skills}
-             iconUrl={this.state.categories[index].categoryImg}
-             categoryName={this.state.categories[index].skillCategoryName}
+           <SkillCategoryLabel
+               id={1}
+               setCategoryDetails={this.setCategoryDetails}
+               skills={this.state.categories[0].skills}
+               iconUrl={this.state.categories[0].categoryImg}
+               categoryName={this.state.categories[0].skillCategoryName}
+               />
+           <SkillCategoryLabel
+               id={2}
+               setCategoryDetails={this.setCategoryDetails}
+               skills={this.state.categories[1].skills}
+               iconUrl={this.state.categories[1].categoryImg}
+               categoryName={this.state.categories[1].skillCategoryName}
+            />
+            <SkillCategoryLabel
+                id={3}
+                setCategoryDetails={this.setCategoryDetails}
+                skills={this.state.categories[2].skills}
+                iconUrl={this.state.categories[2].categoryImg}
+                categoryName={this.state.categories[2].skillCategoryName}
              />
-       }
-       else if (id==4) {
          </div>
          <div style={{display:'block'}} >
            <Lego setLego={this.setLego} currentLego={this.state.lego}/>
          </div>
-       }
-       else{
          <div style={{display:'flex',flexDirection:'column',justifyContent: 'space-between'}} >
          <SkillCategoryLabel
-             id={id}
+             id={4}
              setCategoryDetails={this.setCategoryDetails}
-             skills={this.state.categories[index].skills}
-             iconUrl={this.state.categories[index].categoryImg}
-             categoryName={this.state.categories[index].skillCategoryName}
-             />
-       }
-       </div>
-    )}
-    return (
-    <div>
-      <ReactBootstrap.Grid >
-        <ReactBootstrap.Row >
-          <ReactBootstrap.Col md={12}>
-           <RoleLabel setRoleProps={this.setRoleProps} roleName={this.state.roleName} keywords={this.state.keywords}
-             description={this.state.description} suggestions={this.state.suggestions}
+             skills={this.state.categories[3].skills}
+             iconUrl={this.state.categories[3].categoryImg}
+             categoryName={this.state.categories[3].skillCategoryName}
+          />
+          <SkillCategoryLabel
+              id={5}
+              setCategoryDetails={this.setCategoryDetails}
+              skills={this.state.categories[4].skills}
+              iconUrl={this.state.categories[4].categoryImg}
+              categoryName={this.state.categories[4].skillCategoryName}
            />
-        </ReactBootstrap.Col>
-      </ReactBootstrap.Row>
-      <div style={{display:'flex',flexDirection:'row',justifyContent: 'space-around'}}>
-         {template}
+           <SkillCategoryLabel
+               id={6}
+               setCategoryDetails={this.setCategoryDetails}
+               skills={this.state.categories[5].skills}
+               iconUrl={this.state.categories[5].categoryImg}
+               categoryName={this.state.categories[5].skillCategoryName}
+            />
+         </div>
       </div>
+       <ReactBootstrap.Row >
+        <ReactBootstrap.Col md={6} style={{textAlign:'right',marginTop:'20px'}}>
+          <button  onClick={this.create}>Create Role</button>
+        </ReactBootstrap.Col>
+        <ReactBootstrap.Col md={6} style={{textAlign:'left',marginTop:'20px'}}>
+          <h6 id="msg" style={{color:'red',marginLeft:'5px',backgroundColor:'silver',display:'inline-block'}}>{this.state.error}</h6>
+        </ReactBootstrap.Col>
+       </ReactBootstrap.Row>
       </ReactBootstrap.Grid>
     </div>
     )
