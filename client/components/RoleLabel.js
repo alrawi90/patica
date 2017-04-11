a4d8e9757b45810ba3a692f5951c9a4290b52bf1
Ali Alrawi <ali.developer90@gmail.com>
2017-04-11 17:28:53 +0300

    using inline-edit instead of Modal+inputs for role's name & description

--- client/components/RoleLabel.js
+++ client/components/RoleLabel.js
@@ -1,6 +1,6 @@
 const React = require('react');
 const Role = require('./Role');
-//const ReactTags = require('react-tag-input').WithOutContext;
+import InlineEdit from 'react-edit-inline';
 import { WithContext as ReactTags } from 'react-tag-input';
 class RoleLabel extends React.Component {
   constructor(props) {
@@ -11,20 +11,14 @@ class RoleLabel extends React.Component {
       suggestions:this.props.suggestions,
        key:1
      }
-    this.handler = this.handler.bind(this)
     this.handleDelete = this.handleDelete.bind(this);
     this.handleAddition = this.handleAddition.bind(this);
     this.handleDrag = this.handleDrag.bind(this);
-  }
-  handleClick(e){this.refs.r.openModal(e)}
+    this.RoleNameChanged = this.RoleNameChanged.bind(this);
+    this.DescriptionChanged = this.DescriptionChanged.bind(this);
 
-  handler(rn,description) {
-    this.setState({
-      //RoleName: rn, keywords: keywords ,
-       key: Math.random()
-    })
-    this.props.setRoleProps(rn,this.state.keywords,description)
   }
+
 
   handleDelete(i) {
     this.setState({
@@ -59,20 +53,70 @@ class RoleLabel extends React.Component {
       // re-render
       this.setState({ keywords });
     }
+    RoleNameChanged(data) {
+    // data = { description: "New validated text comes here" }
+    // Update your model from here
+    console.log(data)
+    this.props.setRoleProps(data.message.toUpperCase(),this.state.keywords,this.props.description)
+
+    this.setState({})
+}
+    DescriptionChanged(data){
+      console.log(data)
+      this.props.setRoleProps(this.props.roleName,this.state.keywords,data.message)
+    }
+
+customValidateText(text) {
+  return (text.length > 0 && text.length < 64);
+}
+
   render() {
      const { keywords, suggestions } = this.state;
     return (
 
       <div className="item-role-container">
         <div className="item-role-details">
-          <label className="item-role-name" onClick={(e)=>this.handleClick(e)} >{`ROLE:${this.props.roleName}`}</label><br />
-          <span className='item-role-description' >{this.props.description}</span><br />
+          <InlineEdit
+              validate={this.customValidateText}
+              activeClassName="editing"
+              text={`ROLE:${this.props.roleName}`}
+              paramName="message"
+              change={this.RoleNameChanged}
+              style={{
+                minWidth: 300,
+                display: 'inline-block',
+                margin: 0,
+                padding: 0,
+                fontSize: 18,
+                outline: 0,
+                border: 0
+              }}
+            />
+            </div>
+            <div>
+            <InlineEdit
+                validate={this.customValidateText}
+                activeClassName="editing"
+                text={this.props.description}
+                paramName="message"
+                change={this.DescriptionChanged}
+                style={{
+                  minWidth: 300,
+                  display: 'inline-block',
+                  margin: 0,
+                  padding: 0,
+                  fontSize: 15,
+                  outline: 0,
+                  border: 0
+                }}
+              />
+              <br />
           <ReactTags tags={keywords}
                      suggestions={suggestions}
                      handleDelete={this.handleDelete}
                      handleAddition={this.handleAddition}
                      handleDrag={this.handleDrag} />
-          <Role ref='r' handler={this.handler}  />
+
         </div>
       </div>
 
